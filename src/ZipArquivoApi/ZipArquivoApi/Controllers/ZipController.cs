9835c887c3e57d46bbb5df19441f279477097736
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2023-02-27 22:24:24 -0300

    Controller

---
+++ src/ZipArquivoApi/ZipArquivoApi/Controllers/ZipController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ZipArquivoApi.Controllers
+{
+    [Route("api/zip")]
+    [ApiController]
+    public class ZipController : ControllerBase
+    {
+    }
+}
