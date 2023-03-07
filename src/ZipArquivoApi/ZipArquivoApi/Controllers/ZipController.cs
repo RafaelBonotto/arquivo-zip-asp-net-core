66d0e6a7e7da7462427e78c483c28456df5199e5
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2023-03-06 23:07:53 -0300

    Criando Arquivo Zip

--- src/ZipArquivoApi/ZipArquivoApi/Controllers/ZipController.cs
+++ src/ZipArquivoApi/ZipArquivoApi/Controllers/ZipController.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO.Compression;
 
 namespace ZipArquivoApi.Controllers
 {
@@ -10,7 +10,39 @@ namespace ZipArquivoApi.Controllers
         [HttpGet]
         public async Task<IActionResult> GetZipDocumentos()
         {
-            return Ok();
+            List<byte[]> imagens = new();
+
+            string[] arquivos = Directory.GetFiles(@"C:\teste");
+            byte[] imgdata = System.IO.File.ReadAllBytes(arquivos[0]);
+            
+            string base64Zip = ZipTeste(imagens);
+            return Ok(new { ZipBase64 = base64Zip });
+        }
+        static string ZipTeste(List<byte[]> imgs)
+        {
+            string ret = string.Empty;
+
+            using (var ms = new MemoryStream())
+            {
+                using (var archive = new ZipArchive(ms, ZipArchiveMode.Create, true))
+                {
+                    foreach (var img in imgs)
+                    {
+                        var entry = archive.CreateEntry("Img-3-pgs.pdf", CompressionLevel.Fastest);
+
+                        using (var zipStream = entry.Open())
+                        {
+                            zipStream.Write(img, 0, img.Length);
+                        }
+                    }
+                }
+                //Teste - Salvar Zip no disco
+                System.IO.File.WriteAllBytes(@"C:\\Imagens\\zip3pgs.zip", ms.ToArray());
+                // Final do teste
+
+                ret = Convert.ToBase64String(ms.ToArray());
+            }
+            return ret;
         }
     }
 }
